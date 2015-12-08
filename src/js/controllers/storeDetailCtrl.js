380cd698a110cedea6f6386062aa259f06f92d3c
黄娜珠 <you@example.com>
2015-12-08 16:10:40 +0800

    great

--- src/js/controllers/storeDetailCtrl.js
+++ src/js/controllers/storeDetailCtrl.js
@@ -8,9 +8,10 @@ angular.module('ZJSY_WeChat').controller('StoreDetailController',function($scope
             name : $scope.$parent.storeDetail.storeName,
             startrule : $scope.$parent.storeDetail.start || '30',
             distribution : $scope.$parent.storeDetail.post || '6',
-            intro : $scope.$parent.storeDetail.post || 'the shop is a restaurnt has a long history',
+            intro : $scope.$parent.storeDetail.post || 'the shop is a restaurnt has a long history,' +
+            'they have delious food,sweet cakes and very good wine,many rich people go there and enjoy foods.',
             addr : $scope.$parent.storeDetail.address || 'JinJiHu road 1355 park district',
-            openTime : $scope.$parent.storeDetail.open || '7:00am-10:00pm',
+            openTime : $scope.$parent.storeDetail.open || '7:00AM-10:00PM',
             tel : $scope.$parent.storeDetail.telephone
         }
     })
