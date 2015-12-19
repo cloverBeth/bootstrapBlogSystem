71a1ee5f3fd15b3689cdf41220afe2b54ae9da50
黄娜珠 <you@example.com>
2015-12-19 15:21:12 +0800

    better

--- src/js/controllers/transactionDetailCtrl.js
+++ src/js/controllers/transactionDetailCtrl.js
@@ -1,12 +1,26 @@
 "use strict";
-angular.module('ZJSY_WeChat').controller('TransactionDetailController',function($scope,$state){
+angular.module('ZJSY_WeChat').controller('TransactionDetailController',function($scope,$state,$http){
     $scope.title='交易明细';
-    $scope.transaction=[{
-        number:'20151208092590',
-        order:'666666',
-        cost:'25',
-        order_date:'2015-12-08 00:00:20'
-    }];
+    $scope.transaction=[
+        //number:'20151208092590',
+        //order:'666666',
+        //cost:'25',
+        //order_date:'2015-12-08 00:00:20'
+    ];
+    var orderListApi=X_context.api+"order/list";
+    $http.post(orderListApi,{})
+        .success(function(data){
+            if(!data.data)return;
+            for(var i in data.data){
+                $scope.transaction =[{
+                    number : data.data[i].paidSn,
+                    order : data.data[i].orderSn,
+
+                    order_date : data.data[i].createDate,
+                    cost : data.data[i].totalPrice,}
+                ];}
+})
+
     $scope.goIndex=function(){
         $state.go('store.product',{storeId:X_context.storeId});
     }
