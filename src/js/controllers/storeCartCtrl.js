a6b4805b5e42442b7ca39abc225c447f943c8445
黄娜珠 <you@example.com>
2015-12-23 16:26:09 +0800

    css

--- src/js/controllers/storeCartCtrl.js
+++ src/js/controllers/storeCartCtrl.js
@@ -1,16 +1,12 @@
 "use strict";
 angular.module('ZJSY_WeChat').controller('StoreCartController',function($scope,$http,$stateParams,$state){
-    $scope.$parent.title = "购物车";
+
 
     $scope.cart = $scope.$parent.$parent.cart;
     $scope.storecart={
 
     }
 
-
-    //$scope.username = "陈冠希";
-    //$scope.phone = "13232311009";
-    //$scope.address = "香港XX摄影工作室";
     $scope.$parent.memberPromise.then(function () {
         $http.post(X_context.api + "addr/list", {
             memberId: X_context.memberId,
@@ -28,6 +24,7 @@ angular.module('ZJSY_WeChat').controller('StoreCartController',function($scope,$
                     phone : $scope.storecart.phone,
                     address : $scope.storecart.address
                 });
+                $scope.$parent.title = "购物车";
             })
     });
 
