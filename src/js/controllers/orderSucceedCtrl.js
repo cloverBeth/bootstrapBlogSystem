9fc3a7026d561dfab37cb7e4c59a1d398c0d8878
黄娜珠 <you@example.com>
2015-12-23 18:21:25 +0800

    css

--- src/js/controllers/orderSucceedCtrl.js
+++ src/js/controllers/orderSucceedCtrl.js
@@ -23,9 +23,10 @@ angular.module("ZJSY_WeChat").controller("OrderSucceedController",function($scop
                         && datas[0].paymentMethod == "一卡通"
                         && !datas[0].paidSn) ? "未付款" : datas[0].orderStatus,
             };
-            $scope.title = $scope.orderSucceed.status == "未付款" ? "购物未成功" : "购物成功"
-            if($scope.title==购物未成功){}
-
+            $scope.title = $scope.orderSucceed.status == "未付款" ? "购物未成功" : "购物成功";
+            if($scope.title=="购物未成功"){
+                $("#orderSuccess").css("background-image","url(images/ico_fail.png)");
+            }
         })
 
 
