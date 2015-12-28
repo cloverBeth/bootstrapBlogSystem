fb1d94dbc2af422514ce341bd91b68f603d8068e
黄娜珠 <you@example.com>
2015-12-28 17:38:25 +0800

    ss

--- src/js/controllers/creditAccountCtrl.js
+++ src/js/controllers/creditAccountCtrl.js
@@ -5,7 +5,8 @@ angular.module("ZJSY_WeChat").controller("CreditAccountController",function($sco
        //creditall:558,
        orderSn:null,
 
-   }
+   };
+    $scope.creditdetail = [];
 
 //$scope.creditdetail=[
 //        {
@@ -14,29 +15,33 @@ angular.module("ZJSY_WeChat").controller("CreditAccountController",function($sco
 //        //credit_date:"2013-10-29 20:40",
 //    }
 //]
- var creditListApi=X_context.api+"member/list";
-    $http.post(creditListApi,{
-        id:X_context.memberId
-    })
-        .success(function(data){
-            if(!data.data)return;
-              $scope.credit.creditall= data.data[0].point;
-            var creditAddApi=X_context.api+"order/list";
-            $http.post(creditAddApi,{
-                id:X_context.memberId
-            })
-                .success(function(data){
-                    for(var i in data.data) {
-                        _.forEach(data.data[i].orderSn, function () {
-                            $scope.creditdetail= [{
-                                    creditrule: "购买商品",
-                                    creditval:"+"+data.data[i].userPoint,
-                                    credit_date: data.data[i].createDate,
-                                }]
-                        })
-                    }
-                })
+
+    $scope.$parent.memberPromise.then(function(data){
+        console.log('data',data.data)
+         $scope.credit.creditall= data.data.data[0].point;
+
+        var creditAddApi=X_context.api+"order/list";
+
+        $http.post(creditAddApi,{
+            id:X_context.memberId
         })
+            .success(function(data){
+                for(var i in data.data) {
+                    if(data.data[i].userPoint){
+                        $scope.creditdetail.push({
+                            creditrule: "购买商品",
+                            creditval:"+"+data.data[i].userPoint,
+                            credit_date: data.data[i].createDate,
+                        });
+                    }
+
+                }
+            })
+
+     })
+
+
+
 
 
 
