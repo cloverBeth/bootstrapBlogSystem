75cdfd3d05089577e26de6056f5919ab7be9cd87
黄娜珠 <you@example.com>
2016-01-08 19:56:05 +0800

    service

--- src/js/controllers/serviceOrderCtrl.js
+++ src/js/controllers/serviceOrderCtrl.js
@@ -1,6 +1,9 @@
 "use strict";
 angular.module('ZJSY_WeChat').controller('ServiceOrderController', function($scope){
     $scope.title="服务订单";
+    $(".proState span").click(function(){
+       $(this).parent().next().slideToggle();
+    })
 
 
 })
