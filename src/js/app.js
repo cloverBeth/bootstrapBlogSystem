31c74d9542b71c55914a8442dcafbb9ee81dbe07
黄娜珠 <you@example.com>
2016-01-08 08:51:38 +0800

    gardenart

--- src/js/app.js
+++ src/js/app.js
@@ -353,6 +353,23 @@ angular.module('ZJSY_WeChat', [
                 }
             }
         })
+            .state('gardenArt',{
+                url:'/garden-art',
+                views:{
+                    '':{
+                        templateUrl:'gardenArt.html',
+                        controllerProvider:function($state,$stateParams,checkAuth){
+                            if(!checkAuth.check())return $state.go('login');
+                            var ctrlName="GardenArtController";
+                            return ctrlName;
+                        }
+                    }
+                }
+            })
+
+
+
+
         $urlRouterProvider.otherwise('/store/1/store-product/');
 
 
---
+++ src/js/controllers/gardenArtCtrl.js
@@ -0,0 +1,8 @@
+"use strict";
+angular.module('ZJSY_WeChat').controller('GardenArtController', function($scope){
+    $scope.title="园艺服务";
+
+
+})
+
+
