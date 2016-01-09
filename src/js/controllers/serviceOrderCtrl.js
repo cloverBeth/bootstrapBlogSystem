2f2e31bf42fb9bff800dbf7212255a162bc60187
黄娜珠 <you@example.com>
2016-01-09 14:09:33 +0800

    pics

--- src/js/controllers/serviceOrderCtrl.js
+++ src/js/controllers/serviceOrderCtrl.js
@@ -1,8 +1,16 @@
 "use strict";
 angular.module('ZJSY_WeChat').controller('ServiceOrderController', function($scope){
     $scope.title="服务订单";
+    $scope.service={
+        compName:"云周率",
+        type:" 园艺购买",
+        linkMan:"曹操",
+        linkTel:"15804689644",
+        extraMsg:"寡人穿越至今，竟然看不到一片绿色！！！"
+        };
     $(".proState span").click(function(){
        $(this).parent().next().slideToggle();
+        $(this).toggleClass("togcss");
     })
 
 
