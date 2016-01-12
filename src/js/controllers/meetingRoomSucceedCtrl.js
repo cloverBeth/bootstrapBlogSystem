42f21a7427eb8b8a1977444326c5586fb49b77c0
顾俊 <jayden@yzlpie.com>
2016-01-12 14:03:37 +0800

    bug

---
+++ src/js/controllers/meetingRoomSucceedCtrl.js
@@ -0,0 +1,18 @@
+"use strict";
+angular.module("ZJSY_WeChat").controller("MeetingRoomSucceedController",function($scope,$state,$http,$stateParams){
+    $scope.title="下单成功";
+    $scope.orderSucceed={
+        type:"会议室预订",
+        status:"支付成功",
+        orderNumber:"201976875613623",
+        expense:"300",
+        payway:"线下支付",
+        telphone:"1796733266"
+    }
+
+
+    $scope.goToOnline=function(){
+        $state.go('onlineService');
+    }
+
+})
