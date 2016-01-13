a670c3943f1103332d6dbe481d141820ed99d9e8
顾俊 <jayden@yzlpie.com>
2016-01-13 17:08:50 +0800

    activity

--- src/js/controllers/activityDetailCtrl.js
+++ src/js/controllers/activityDetailCtrl.js
@@ -1,4 +1,4 @@
 "use strict";
-angular.module('ZJSY_WeChat').controller('ActivityDetailController',function($scope,$state,$http){
-
+angular.module('ZJSY_WeChat').controller('ActivityDetailController',function($scope,$state,$http,$sce){
+    $scope.embedHtml = $sce.trustAsHtml('<div><span style="color: rgb(255, 153, 0);"><span style="background-color: rgb(102, 185, 102);">T</span></span><img src="http://quilljs.com/images/cloud.png" style="color: rgb(255, 153, 0); background-color: rgb(102, 185, 102);"><span style="color: rgb(255, 153, 0);"><span style="background-color: rgb(102, 185, 102);">est1</span></span></div>');
 })
