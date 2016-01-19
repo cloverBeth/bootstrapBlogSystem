09050628f512642453d6eef2ac55d8e57796dac2
eroszzh <zhenhzhu@126.com>
2016-01-19 16:42:28 +0800

    add my coupon page

--- src/js/app.js
+++ src/js/app.js
@@ -527,7 +527,19 @@ angular.module('ZJSY_WeChat', [
                     }
                 }
             }
-        })
+        }).state('myCoupon',{
+                url:'/myCoupon',
+                views:{
+                    '':{
+                        templateUrl:'myCoupon.html',
+                        controllerProvider: function($state,$stateParams,checkAuth) {
+                            if(!checkAuth.check())return $state.go('login');
+                            var ctrlName = "MyCouponController";
+                            return ctrlName;
+                        }
+                    }
+                }
+            })
 
 
         $urlRouterProvider.otherwise('/store/1/store-product/');
---
+++ src/js/controllers/myCouponCtrl.js
@@ -0,0 +1,60 @@
+"use strict";
+angular.module('ZJSY_WeChat').controller('MyCouponController',function($scope,$http){
+    "use strict";
+
+    //temp
+    var X_context = {};
+    X_context.api = 'http://localhost:8080/zjsy/api/v1/';
+
+    $scope.title = '我的优惠券';
+
+    $scope.canLoad = true;
+    $scope.maxItems = 100;
+    $scope.currentPage = 1;
+    $scope.pageSize = 8;
+    $scope.pageNum = null;
+
+    $scope.couponList = [];
+
+    $scope.blueStatus = ["未使用"];
+
+
+    $scope.init = function() {
+        $scope.listCoupon();
+    };
+
+    $scope.addItems = function () {
+        $scope.currentPage++;
+        if($scope.currentPage > $scope.pageNum || !$scope.canLoad) {
+            $scope.canLoad = false;
+        } else {
+            $scope.listCoupon();
+        }
+    };
+
+    $scope.listCoupon = function() {
+        $http.post(X_context.api + 'coupon/mine',{
+            storeId : $scope.$parent.storeId,
+            page : $scope.currentPage,
+            size : $scope.pageSize
+        }).success(function (resp) {
+            var res = resp.data;
+            $scope.couponList = $scope.couponList.concat(res.coupon);
+            $scope.maxItems = res.page.total;
+            $scope.pageNum = ($scope.maxItems && $scope.couponList.length >= 1)
+                ? Math.ceil($scope.maxItems / $scope.pageSize)
+                : 1;
+        });
+    };
+
+    $scope.checkBlue = function(status, background) {
+        if(background) {
+            return _.includes($scope.blueStatus, status)? 'blue' : 'gray';
+        } else {
+            return _.includes($scope.blueStatus, status)? 'white' : 'gray';
+        }
+
+    };
+
+
+});
