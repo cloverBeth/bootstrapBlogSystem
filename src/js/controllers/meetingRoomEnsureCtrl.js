15e605927271b1c51cc99916e37062249b662bc0
黄娜珠 <you@example.com>
2016-01-19 13:05:37 +0800

    coppy

--- src/js/controllers/meetingRoomEnsureCtrl.js
+++ src/js/controllers/meetingRoomEnsureCtrl.js
@@ -13,7 +13,7 @@ angular.module('ZJSY_WeChat').controller('MeetingRoomEnsureController', function
     $scope.price = $stateParams.meetingOrder.price;
     $scope.room = $stateParams.meetingOrder.room;
     $scope._ = _;
-    //$scope.payMethod = 'cash';
+    $scope.payMethod = 'cash';
     //$scope.user = "";
     //$scope.phone = "";
     //$scope.company = "";
@@ -32,30 +32,20 @@ angular.module('ZJSY_WeChat').controller('MeetingRoomEnsureController', function
         $http.post(X_context.api + 'meeting/rentRoom',
             {
                 roomid : $scope.room.id,
-                "contact" : $scope.user,
-                "remark" : $scope.memo,
-                "mobile" : $scope.phone,
-                "company" : $scope.company,
-                "payamount" : $scope.price,
-                "paytype" : $scope.payMethod,
                 meetingdate : `${$scope.date.getFullYear()}-${$scope.date.getMonth()+1}-${$scope.date.getDate()}`,
-                meetingtime : _.pluck($scope.time,'name').join(','),
-<<<<<<< HEAD
-                memberid : X_context.memberId
-=======
-                memberid : X_context.memberId,
-                contact : $scope.user,
-                remark : $scope.memo,
-                mobile : $scope.phone,
-                company : $scope.company,
-                paytype : false
->>>>>>> d430bd1cd29c810b7b95e7e351664f526285e8fa
+        meetingtime : _.pluck($scope.time,'name').join(','),
+            memberid : X_context.memberId,
+            contact : $scope.user,
+            remark : $scope.memo,
+            mobile : $scope.phone,
+            company : $scope.company,
+            paytype : false
 
-            }).success(function(data){
-                $state.go('meetingRoomSucceed',{orderId:data.data[0].orderId});
+    }).success(function(data){
+        $state.go('meetingRoomSucceed',{orderId:data.data[0].orderId});
 
-            })
-    };
+    })
+};
 
     $scope.goEnsureAndPay = function(){
         if(!$scope.user || !$scope.phone || !$scope.company){
@@ -66,15 +56,15 @@ angular.module('ZJSY_WeChat').controller('MeetingRoomEnsureController', function
             {
                 roomid : $scope.room.id,
                 meetingdate : `${$scope.date.getFullYear()}-${$scope.date.getMonth()+1}-${$scope.date.getDate()}`,
-                meetingtime : _.pluck($scope.time,'name').join(','),
-                memberid : X_context.memberId,
-                contact : $scope.user,
-                remark : $scope.memo,
-                mobile : $scope.phone,
-                company : $scope.company,
-                paytype : true
-            }).success(function(data){
-                $state.go('cardLogin',{from:{fromMeeting : true,orderId : data.data[0].orderId}});
-            })
+        meetingtime : _.pluck($scope.time,'name').join(','),
+            memberid : X_context.memberId,
+            contact : $scope.user,
+            remark : $scope.memo,
+            mobile : $scope.phone,
+            company : $scope.company,
+            paytype : true
+    }).success(function(data){
+        $state.go('cardLogin',{from:{fromMeeting : true,orderId : data.data[0].orderId}});
+        })
     }
 })
