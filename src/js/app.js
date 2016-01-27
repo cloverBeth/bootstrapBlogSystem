9e93a6a5547085f69bccbbe9b702b76a1fc569c3
顾俊 <jayden@yzlpie.com>
2016-01-27 10:32:50 +0800

    bug

--- src/js/app.js
+++ src/js/app.js
@@ -670,7 +670,7 @@ angular.module('ZJSY_WeChat', [
             }
         });
 
-        $urlRouterProvider.otherwise('/store/1/store-product/');
+        //$urlRouterProvider.otherwise('/store/1/store-product/');
 
 
     });
