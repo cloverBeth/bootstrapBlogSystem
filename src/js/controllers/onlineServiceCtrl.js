ecc0c7d144309f392cfb200cbb03c74c4fb1873d
黄娜珠 <you@example.com>
2016-01-28 15:23:31 +0800

    bugs

--- src/js/controllers/onlineServiceCtrl.js
+++ src/js/controllers/onlineServiceCtrl.js
@@ -1,5 +1,17 @@
 "use strict";
-angular.module('ZJSY_WeChat').controller('OnlineServiceController', function($rootScope,$scope,$state) {
+angular.module('ZJSY_WeChat').controller('OnlineServiceController', function($rootScope,$scope,$http,$state) {
     $scope.title = "线上物业";
+
+
+    $scope.$parent.memberPromise.then(function() {
+
+        $http.post(X_context.api + "services/list", {
+            "memberid": X_context.memberId,
+            "servicesId": 1
+
+        }).success(function(data){
+            $scope.onlineTel=data.data[0].mobile;
+        })
+    })
 
 })
