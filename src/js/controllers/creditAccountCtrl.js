b73215cecdc86d1f289358ab45d185f2bdafcdd7
黄娜珠 <you@example.com>
2016-01-28 19:28:12 +0800

    decrease credit

--- src/js/controllers/creditAccountCtrl.js
+++ src/js/controllers/creditAccountCtrl.js
@@ -2,44 +2,33 @@
 angular.module("ZJSY_WeChat").controller("CreditAccountController",function($scope,$state,$http,$stateParams){
    $scope.credit={
        title:"我的积分",
-       //creditall:558,
        orderSn:null,
 
    };
     $scope.creditdetail = [];
 
-//$scope.creditdetail=[
-//        {
-//        creditrule:"购买商品",
-//        //creditval:34,
-//        //credit_date:"2013-10-29 20:40",
-//    }
-//]
-
     $http.get(X_context.api + 'member/getCurMem')
         .success(function(data){
-            console.log('data',data.data);
 
             $scope.credit.creditall= data.data[0].point;
+            console.log('pointLeft',data.data[0].point);
 
+            $http.post(X_context.api + "order/list",{
+                id: X_context.memberId,
+                //userPointDone : 1
+            }).success(function(data){
 
-            var creditAddApi=X_context.api+"order/list";
-            $http.post(creditAddApi,{
-                id:X_context.memberId,
-                userPointDone : 1
+                for(var i in data.data) {
+                    $scope.creditdetail.push({
+                    creditrule:data.data[i].storeName=="积分商城"? "积分抵用" : "购买商品",
+                    creditval:data.data[i].storeName=="积分商城"? "-"+data.data[i].point : "+"+data.data[i].userPoint,
+                    credit_date: data.data[i].createDate,
+                  });
+
+                }
+
             })
-                .success(function(data){
-                    for(var i in data.data) {
-                        if(data.data[i].userPoint){
-                            $scope.creditdetail.push({
-                                creditrule: "购买商品",
-                                creditval:"+"+data.data[i].userPoint,
-                                credit_date: data.data[i].createDate,
-                            });
-                        }
 
-                    }
-                })
         });
 
 });
