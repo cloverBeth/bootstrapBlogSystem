48bca031f99928656272286dfe1a7df1de75e24f
黄娜珠 <you@example.com>
2016-01-30 15:58:33 +0800

    bugs

--- src/js/controllers/creditAccountCtrl.js
+++ src/js/controllers/creditAccountCtrl.js
@@ -17,12 +17,13 @@ angular.module("ZJSY_WeChat").controller("CreditAccountController",function($sco
                 id: X_context.memberId,
                 //userPointDone : 1
             }).success(function(data){
+                //console.log(data.data[0].point);
 
                 for(var i in data.data) {
                     $scope.creditdetail.push({
                     credit_date: data.data[i].createDate,
                     creditrule:data.data[i].storeName=="积分商城"? "积分抵用" : "购买商品",//如果是积分商城就显示积分抵用
-                    creditval:data.data[i].point && data.data[i].storeName=="积分商城" ? "-"+data.data[i].point : "+"+data.data[i].userPoint,
+                    creditval:!data.data[i].point && data.data[i].storeName!="积分商城" ? "+"+data.data[i].userPoint : "-"+data.data[i].point,
 
                   });
 
