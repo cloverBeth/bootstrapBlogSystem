4848d3deb2a64ac3ae3ae70cf10b66adca82d575
SunnyPig <15851412061@163.com>
2016-03-08 15:46:13 +0800

    ch

--- src/js/controllers/accountCenterCtrl.js
+++ src/js/controllers/accountCenterCtrl.js
@@ -2,6 +2,8 @@ angular.module('ZJSY_WeChat')
     .controller('AccountCenterController', function($scope,$location,$http,$rootScope) {
         "use strict";
 
-
+        $scope.hoverOver=function(){
+            alert('aha');
+        }
 
     })
